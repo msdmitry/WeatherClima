e0a4b1891c29ff0583a225f143791cd9490ee2ff
msdmitry <mishin.grand@gmail.com>
2022-07-19 09:47:07 +0300

    Initial Commit

---
+++ WeatherClima/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherClima
+//
+//  Created by Dmitry M. on 7/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
